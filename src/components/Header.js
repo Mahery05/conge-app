38e49226fa80b9150cd84ecc0359a3d06a2a0d11
mrazakandraina <mrazakandraina@optilian.com>
2024-08-02 17:00:53 +0200

    modification du header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <header className="bg-blue-500 text-white p-4 flex justify-between items-center">
       <div className="flex items-center">
-        <img src="public\dembele.jpg" alt="Logo" className="h-8 w-8 mr-2" />
+        <img src="paris_saint_germain.png" alt="Logo" className="h-8 w-8 mr-2" />
         <h1 className="text-lg font-bold">Congé Mahery Razakandraina</h1>
       </div>
       <div className="flex items-center">
-        <img src="public\dembele.jpg" alt="User" className="h-8 w-8 rounded-full mr-2" />
+        <img src="dembele.jpg" alt="User" className="h-8 w-8 rounded-full mr-2" />
         <button className="bg-red-500 px-4 py-2 rounded">Déconnexion</button>
       </div>
     </header>
