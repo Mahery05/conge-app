115a9382ba7b947c5df1359211679a31b087016e
Mahery05 <mahery.razakandraina@gmail.com>
2024-08-04 16:51:03 +0200

    modification de compteur de congés

--- src/components/Count.js
+++ src/components/Count.js
@@ -4,8 +4,8 @@ const Count = () => {
   return (
     <div className="bg-white p-4 rounded shadow-md mb-4">
       <h2 className="text-xl font-bold mb-2">Récapitulatif des Congés</h2>
-      <p>Jours posés cette année : 10</p>
-      <p>Jours restants : 15</p>
+      <p>Jours posés cette année : 8</p>
+      <p>Jours restants : 5</p>
     </div>
   );
 };
