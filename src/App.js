af4f7904933a71c0c0a9986043fcdf671ad52231
Mahery05 <mahery.razakandraina@gmail.com>
2024-08-04 16:30:11 +0200

    ajout de calendrier dans l'app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import Header from './components/Header';
-import Compteur from './components/Compteur';
-import ListeConges from './components/ListeConges';
-import FormulaireConge from './components/FormulaireConge';
+import Count from './components/Count';
+import List from './components/List';
+import Form from './components/Form';
+import Calendar from 'react-calendar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const conges = [
+    { date: '04/08/2024', status: 'Approuvé' },
+    { date: '12/08/2024', status: 'Approuvé' },
+    { date: '05/05/2025', status: 'En attente' },
+  ];
+
+  const handleCongesSubmit = () => {
+    toast.success('Demande de congé soumise avec succès!');
+  };
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
       <div className="container mx-auto p-4">
-        <Compteur />
-        <ListeConges />
-        <FormulaireConge />
+        <Count />
+        <List />
+        <Form onSubmit={handleCongesSubmit} />
+        <Calendar conges={conges} />
       </div>
     </div>
   );
